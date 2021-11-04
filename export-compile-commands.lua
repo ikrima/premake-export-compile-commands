9cc3ce6c532be390916fec8d4516844010d4df24
ikrima <contact@ikrima.com>
2021-11-04 17:52:23 -0500

    more clang-cl shims

--- export-compile-commands.lua
+++ export-compile-commands.lua
@@ -24,8 +24,71 @@ local workspace = p.workspace
 local project = p.project
 
 function m.getToolset(cfg)
-  if     string.startswith(cfg.toolset, "msc") then return p.tools["clang"]
-  else                                         return  p.tools[cfg.toolset] end
+  if string.startswith(cfg.toolset, "msc") then
+    -- Shims for msc toolset to play nice with clang-cl
+    local msc_orig = p.tools.msc
+    local toolset = {}
+    toolset.version = msc_orig.version
+    toolset.gettoolname = function(cfg,tool)
+      if tool == "cc" or tool == "cxx" then
+        return "clang-cl" end
+      return nil
+    end
+    toolset.getcppflags = msc_orig.getcppflags
+    toolset.getdefines = msc_orig.getdefines
+    toolset.getundefines = msc_orig.getundefines
+    toolset.getincludedirs = msc_orig.getincludedirs
+    toolset.getforceincludes = msc_orig.getforceincludes
+    toolset.getcxxflags = function(cfg)
+      local clang_cxxflags = {
+        cppdialect = {
+          ["C++98"]     = "/std:c++98",
+          ["C++0x"]     = "/std:c++0x",
+          ["C++11"]     = "/std:c++11",
+          ["C++1y"]     = "/std:c++1y",
+          ["C++14"]     = "/std:c++14",
+          ["C++1z"]     = "/std:c++1z",
+          ["C++17"]     = "/std:c++17",
+          ["C++2a"]     = "/std:c++2a",
+          ["C++20"]     = "/std:c++20",
+          ["gnu++98"]   = "/std:gnu++98",
+          ["gnu++0x"]   = "/std:gnu++0x",
+          ["gnu++11"]   = "/std:gnu++11",
+          ["gnu++1y"]   = "/std:gnu++1y",
+          ["gnu++14"]   = "/std:gnu++14",
+          ["gnu++1z"]   = "/std:gnu++1z",
+          ["gnu++17"]   = "/std:gnu++17",
+          ["gnu++2a"]   = "/std:gnu++2a",
+          ["gnu++20"]   = "/std:gnu++20",
+          ["C++latest"] = "/std:c++20",
+        }
+      }
+      local flags =  msc_orig.getcxxflags(cfg)
+      flags = table.join(flags, p.config.mapFlags(cfg, clang_cxxflags))
+      return flags
+    end
+    toolset.getcflags = function(cfg)
+      local clang_cflags = {
+        cdialect = {
+          ["C89"]   = "/std:c89",
+          ["C90"]   = "/std:c90",
+          ["C99"]   = "/std:c99",
+          ["C11"]   = "/std:c11",
+          ["gnu89"] = "/std:gnu89",
+          ["gnu90"] = "/std:gnu90",
+          ["gnu99"] = "/std:gnu99",
+          ["gnu11"] = "/std:gnu11",
+        }
+      }
+		  local flags = msc_orig.getcflags(cfg)
+      flags = table.join(flags,p.config.mapFlags(cfg, clang_cflags))
+      return flags
+    end
+    return toolset
+  else
+      local tool, version = p.config.toolset(cfg)
+      return tool
+  end
 end
 
 function m.getCompileLang(prj, node)
@@ -40,11 +103,10 @@ end
 
 function m.getToolName(prj, cfg, node)
   local compileLang = m.getCompileLang(prj,node)
-  local tool, version = p.config.toolset(cfg)
-  if     string.startswith(cfg.toolset, "msc") then return "clang-cl"
-  elseif compileLang == "cpp"                  then return tool.gettoolname(cfg, "cxx")
-  elseif compileLang == "c"                    then return tool.gettoolname(cfg, "cc")
-  else                                         error("Invalid file: " .. node.abspath) end
+  local tool = m.getToolset(cfg)
+  if     compileLang == "cpp" then return tool.gettoolname(cfg, "cxx")
+  elseif compileLang == "c"   then return tool.gettoolname(cfg, "cc")
+  else                        error("Invalid file: " .. node.abspath) end
 end
 
 -- function m.getIncludeDirs(cfg)
@@ -77,6 +139,7 @@ function m.getConfigFlags(prj, projCfg, node)
   local toolset = m.getToolset(projCfg)
 
   local function perConfigFlags(fileCfg_or_projCfg)
+    if not fileCfg_or_projCfg then return {} end
     local retFlags = {}
     retFlags = table.join(retFlags, toolset.getdefines(fileCfg_or_projCfg.defines))
     retFlags = table.join(retFlags, toolset.getundefines(fileCfg_or_projCfg.undefines))
@@ -95,9 +158,7 @@ function m.getConfigFlags(prj, projCfg, node)
   end
   local flags = toolset.getcppflags(projCfg)
   flags = table.join(flags, perConfigFlags(projCfg))
-  local fcfg = p.fileconfig.getconfig(node, projCfg)
-  if fcfg then flags = table.join(flags, perConfigFlags(fcfg)) end
-
+  flags = table.join(flags, perConfigFlags(p.fileconfig.getconfig(node, projCfg)))
   project.getrelative = old_getrelative
   return flags
 end
